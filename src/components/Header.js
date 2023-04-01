b9d421cd253b7994787d59ed76c391838abcd54b
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-01 09:48:36 +0530

    fix: fix styling and added media query for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+
 import CompanyLogo from "../assets/images/logo-transparent.png";
 
 import "./Header.scss";
@@ -9,11 +11,15 @@ const Header = () => {
       <div className="logo__box">
         <img src={CompanyLogo} alt="logo" class="logo" />
       </div>
+
       <div className="header__container">
         <p className="header__item">About</p>
         <p className="header__item">Experience</p>
         <p className="header__item">Projects</p>
         <p className="header__item">Contact</p>
+      </div>
+      <div className="menubar__container">
+        <FaBars className="menubar__icon" />
       </div>
     </div>
   );
