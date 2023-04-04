149b3c1cba20f04c15e2609ca8a6f34160e765ca
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-04 17:50:40 +0530

    Added: Implemented router in project

---
+++ src/pages/NewPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>This is new page</h1>
+    </div>
+  );
+};
+
+export default NewPage;
