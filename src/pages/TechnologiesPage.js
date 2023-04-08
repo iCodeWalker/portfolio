add6e7fe34a9c8b19271404dc49350f3edb92c00
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-08 19:33:34 +0530

    fix: Removed tech section image and fixed align

--- src/pages/TechnologiesPage.js
+++ src/pages/TechnologiesPage.js
@@ -9,7 +9,9 @@ const TechnologiesPage = () => {
     <div className="technologies-section">
       <div className="technologies-section__text-container">
         <div>
-          <p className=" technologies-section__heading">Technologies Worked:</p>
+          <p className=" technologies-section__heading">
+            Technologies Worked With:
+          </p>
           <div className="technologies-section__text">
             <p className="technologies-section__text-item">React</p>
             <p className="technologies-section__text-item">React Native</p>
@@ -18,26 +20,27 @@ const TechnologiesPage = () => {
             <p className="technologies-section__text-item">CSS3</p>
             <p className="technologies-section__text-item">JavaScript</p>
             <p className="technologies-section__text-item">TypeScript</p>
+            <p className="technologies-section__text-item">Dart</p>
             <p className="technologies-section__text-item">jQuery</p>
             <p className="technologies-section__text-item">Figma</p>
-            <p className="technologies-section__text-item">NodeJS</p>
-            <p className="technologies-section__text-item">ExpressJS</p>
           </div>
         </div>
         <div>
           <p className=" technologies-section__heading">Have Knowledge of:</p>
           <div className="technologies-section__text">
             <p className="technologies-section__text-item">NodeJS</p>
+            <p className="technologies-section__text-item">ExpressJS</p>
             <p className="technologies-section__text-item">Next JS</p>
             <p className="technologies-section__text-item">Django</p>
             <p className="technologies-section__text-item">MongoDB</p>
             <p className="technologies-section__text-item">GraphQL</p>
+            <p className="technologies-section__text-item">Firebase</p>
           </div>
         </div>
       </div>
-      <div className="technologies-section__image-container">
+      {/* <div className="technologies-section__image-container">
         <RotatingSphere />
-      </div>
+      </div> */}
     </div>
   );
 };
